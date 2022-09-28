91db66b99a9d3d7a2251039133e4a42fae5ba880
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2022-09-28 09:14:09 -0600

    Finished Tarea 7

---
+++ twcam/tarea7-bootstrap-javascript/index.js
@@ -0,0 +1,71 @@
+const activar = (option) => {
+  document
+    .querySelectorAll(".nav-item .nav-link")
+    .forEach((nl) => nl.classList.remove("active"));
+  option.classList.add("active");
+};
+
+let carrito = {};
+
+const comprar = (producto) => {
+  const id = producto.getAttribute("id");
+  const nombre = producto.querySelector(".nombreProducto").innerHTML;
+  const precio = Number(producto.querySelector(".precioProducto").innerHTML);
+  const cantidad = Number(producto.querySelector(".inputCantidad").value);
+
+  if (carrito[id]) {
+    carrito[id].cantidad += cantidad;
+  } else {
+    carrito[id] = {
+      nombre,
+      precio,
+      cantidad,
+    };
+  }
+
+  actualizarCarrito();
+  actualizarResumen();
+};
+
+const actualizarCarrito = () => {
+  const lista = document.getElementById("productos");
+  lista.innerHTML = "";
+
+  Object.keys(carrito).forEach((producto) => {
+    lista.innerHTML += crearElemento(carrito[producto], producto);
+  });
+};
+
+const crearElemento = (item, index) => {
+  const { nombre, precio, cantidad } = item;
+  return `<li id="carrito-${index}">
+    <div class="form-inline">
+      <div>${nombre} - ${cantidad} - ${precio} <a href="#" onclick="borrar(\'${index}\')">&nbsp;x</a></div>
+    </div>
+  </li>`;
+};
+
+const actualizarResumen = () => {
+  const productKeys = Object.keys(carrito);
+
+  const cantidad = productKeys.reduce(
+    (prev, curr) => prev + carrito[curr].cantidad,
+    0
+  );
+
+  const total = productKeys.reduce(
+    (prev, curr) => prev + carrito[curr].precio * carrito[curr].cantidad,
+    0
+  );
+
+  document.getElementById(
+    "resumen"
+  ).innerHTML = `Productos ${cantidad} | Precio ${total}€`;
+};
+
+const borrar = (id) => {
+  if (carrito[id]) delete carrito[id];
+
+  actualizarCarrito();
+  actualizarResumen();
+};
---
+++ twcam/tarea7-bootstrap-javascript/readme.txt
@@ -0,0 +1,7 @@
+Tarea 7 - Tema 1
+Wilfredo Rene Paiz Paiz
+
+Repositorio: https://github.com/wpayze/wpayze.github.io/tree/master/twcam/tarea7-bootstrap-javascript
+Resultado final: https://wpayze.github.io/twcam/tarea7-bootstrap-javascript/
+
+Codigo adjunto en la carpeta "código"
